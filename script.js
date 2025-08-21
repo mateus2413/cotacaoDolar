37af1bb00f81deaa9d99d444fdedb965ca4882e6
mateus2413 <mjustino468@gmail.com>
2025-08-20 21:03:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//conectar com API dp Dólar simulando um NO-SQL
+
+fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL').then(resposta => {
+     return resposta.json()
+}).then(economia => {
+    console.log(economia)
+
+    document.getElementById("valorDolar").innerHTML = "R$" + economia.USDBRL.bid
+ })        
